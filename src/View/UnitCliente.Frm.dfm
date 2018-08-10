5233e6f466e1985032b67e0295b4f7d806243b4c
lucas.guimaraes <lucasasafe@outlook.com.br>
2018-08-10 00:23:12 -0300

    Tela de cadastro de clientes

---
+++ src/View/UnitCliente.Frm.dfm
@@ -0,0 +1,232 @@
+inherited Form3: TForm3
+  ActiveControl = Edit1
+  Caption = 'Form3'
+  OnCreate = FormCreate
+  PixelsPerInch = 96
+  TextHeight = 13
+  object Label1: TLabel [0]
+    Left = 8
+    Top = 260
+    Width = 24
+    Height = 13
+    Caption = 'Sexo'
+  end
+  object Label2: TLabel [1]
+    Left = 138
+    Top = 260
+    Width = 55
+    Height = 13
+    Caption = 'Nascimento'
+  end
+  object Label3: TLabel [2]
+    Left = 241
+    Top = 260
+    Width = 44
+    Height = 13
+    Caption = 'Cadastro'
+  end
+  object Label4: TLabel [3]
+    Left = 360
+    Top = 260
+    Width = 35
+    Height = 13
+    Caption = 'Cr'#233'dito'
+  end
+  object Label5: TLabel [4]
+    Left = 8
+    Top = 212
+    Width = 45
+    Height = 13
+    Caption = 'Endere'#231'o'
+  end
+  object Label6: TLabel [5]
+    Left = 8
+    Top = 165
+    Width = 33
+    Height = 13
+    Caption = 'C'#243'digo'
+  end
+  object Label7: TLabel [6]
+    Left = 87
+    Top = 165
+    Width = 27
+    Height = 13
+    Caption = 'Nome'
+  end
+  object Label8: TLabel [7]
+    Left = 323
+    Top = 165
+    Width = 54
+    Height = 13
+    Caption = 'Documento'
+  end
+  object Label9: TLabel [8]
+    AlignWithMargins = True
+    Left = 3
+    Top = 3
+    Width = 440
+    Height = 19
+    Align = alTop
+    Caption = 'Busca: Tabela Clientes'
+    Font.Charset = DEFAULT_CHARSET
+    Font.Color = clWindowText
+    Font.Height = -16
+    Font.Name = 'Tahoma'
+    Font.Style = []
+    ParentFont = False
+    ExplicitWidth = 158
+  end
+  inherited btnCancelar: TButton
+    Top = 330
+    TabOrder = 8
+    ExplicitTop = 330
+  end
+  inherited btnNovo: TButton
+    Top = 330
+    TabOrder = 9
+    ExplicitTop = 330
+  end
+  inherited Panel1: TPanel
+    Top = 408
+    Height = 72
+    TabOrder = 10
+    ExplicitTop = 408
+    ExplicitHeight = 72
+    inherited ListBox1: TListBox
+      Height = 54
+      ExplicitHeight = 54
+    end
+  end
+  inherited btnSalvar: TButton
+    Top = 330
+    TabOrder = 11
+    ExplicitTop = 330
+  end
+  inherited btnExcluir: TButton
+    Top = 330
+    TabOrder = 12
+    ExplicitTop = 330
+  end
+  inherited btnEditar: TButton
+    Top = 330
+    TabOrder = 13
+    ExplicitTop = 330
+  end
+  inherited StringGrid: TStringGrid
+    Top = 58
+    Height = 101
+    Options = [goFixedVertLine, goFixedHorzLine, goVertLine, goHorzLine, goColSizing, goTabs, goRowSelect, goThumbTracking, goFixedColClick, goFixedRowClick, goFixedHotTrack]
+    TabOrder = 14
+    OnSelectCell = StringGridSelectCell
+    ExplicitTop = 58
+    ExplicitHeight = 101
+    RowHeights = (
+      24
+      25)
+  end
+  object edCodigo: TEdit
+    Left = 8
+    Top = 184
+    Width = 73
+    Height = 21
+    NumbersOnly = True
+    TabOrder = 0
+  end
+  object edNome: TEdit
+    Left = 87
+    Top = 184
+    Width = 230
+    Height = 21
+    TabOrder = 1
+  end
+  object edDocumento: TEdit
+    Left = 323
+    Top = 184
+    Width = 115
+    Height = 21
+    TabOrder = 2
+  end
+  object edEndereco: TEdit
+    Left = 8
+    Top = 231
+    Width = 430
+    Height = 21
+    TabOrder = 3
+  end
+  object edCredito: TEdit
+    Left = 360
+    Top = 279
+    Width = 78
+    Height = 21
+    NumbersOnly = True
+    TabOrder = 4
+  end
+  object cbSexo: TComboBox
+    Left = 8
+    Top = 279
+    Width = 124
+    Height = 21
+    TabOrder = 5
+    Items.Strings = (
+      'Masculino'
+      'Feminino')
+  end
+  object dtDataNascimento: TJvDateTimePicker
+    Left = 138
+    Top = 280
+    Width = 97
+    Height = 21
+    Date = 0.800771655092830700
+    Time = 0.800771655092830700
+    DoubleBuffered = False
+    ParentDoubleBuffered = False
+    TabOrder = 6
+    DropDownDate = 42599.000000000000000000
+    NullText = '00/00/0000'
+  end
+  object dtDataCadastro: TJvDateTimePicker
+    Left = 241
+    Top = 280
+    Width = 113
+    Height = 21
+    Date = 0.800771655092830700
+    Time = 0.800771655092830700
+    TabOrder = 7
+    DropDownDate = 42565.000000000000000000
+    NullText = '00/00/0000'
+  end
+  object Edit1: TEdit
+    AlignWithMargins = True
+    Left = 3
+    Top = 28
+    Width = 440
+    Height = 24
+    Align = alTop
+    Font.Charset = DEFAULT_CHARSET
+    Font.Color = clRed
+    Font.Height = -13
+    Font.Name = 'Courier New'
+    Font.Style = [fsBold]
+    ParentFont = False
+    TabOrder = 15
+    OnKeyUp = Edit1KeyUp
+  end
+  object Button1: TButton
+    Left = 8
+    Top = 361
+    Width = 169
+    Height = 25
+    Caption = 'Gerar Relat'#243'rio'
+    TabOrder = 16
+    OnClick = Button1Click
+  end
+  object Button2: TButton
+    Left = 183
+    Top = 361
+    Width = 167
+    Height = 25
+    Caption = '>> Exportar <<'
+    TabOrder = 17
+    OnClick = Button2Click
+  end
+end
---
+++ src/View/UnitCliente.Frm.pas
@@ -0,0 +1,205 @@
+unit UnitCliente.Frm;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes,
+  Vcl.Graphics, Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, Vcl.ComCtrls,
+  JvExComCtrls, JvDateTimePicker, Data.DB, Vcl.ExtCtrls, Vcl.Grids, Vcl.DBGrids,
+  UnitDataModule, UnitClass.PersistenciaClass, UnitClass.Validador, UnitCliente,
+  UnitClass.FactoryForm, UnitBase.Frm, UnitRTTIExport;
+
+type
+  TForm3 = class(TFormBase)
+    edCodigo: TEdit;
+    edNome: TEdit;
+    edDocumento: TEdit;
+    edEndereco: TEdit;
+    edCredito: TEdit;
+    cbSexo: TComboBox;
+    Label1: TLabel;
+    Label2: TLabel;
+    Label3: TLabel;
+    Label4: TLabel;
+    Label5: TLabel;
+    Label6: TLabel;
+    Label7: TLabel;
+    Label8: TLabel;
+    dtDataNascimento: TJvDateTimePicker;
+    dtDataCadastro: TJvDateTimePicker;
+    Label9: TLabel;
+    Edit1: TEdit;
+    Button1: TButton;
+    Button2: TButton;
+    procedure btnSalvarClick(Sender: TObject);
+    procedure btnCancelarClick(Sender: TObject);
+    procedure btnNovoClick(Sender: TObject);
+    procedure btnExcluirClick(Sender: TObject);
+    procedure FormShow(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
+    procedure StringGridSelectCell(Sender: TObject; ACol, ARow: Integer; var CanSelect: Boolean);
+    procedure btnEditarClick(Sender: TObject);
+    procedure Edit1KeyUp(Sender: TObject; var Key: Word; Shift: TShiftState);
+    procedure Button1Click(Sender: TObject);
+    procedure Button2Click(Sender: TObject);
+  private
+    FClientes: TClientes;
+    FPercistenceClass: TPersistenciaClass;
+    procedure atualizaGrid(selRow: Integer = 1);
+    function getRegistroFromClienteList(LinhaGrid: Integer): TCliente;
+  public
+    property Clientes: TClientes read FClientes write FClientes;
+  end;
+
+var
+  Form3: TForm3;
+
+implementation
+
+{$R *.dfm}
+
+uses
+  UnitClass.FactoryClass;
+
+procedure TForm3.atualizaGrid(selRow: Integer = 1);
+var
+  c: TCliente;
+begin
+  StringGrid.RowCount := FClientes.Count + 1;
+
+  StringGrid.Cols[0].Clear;
+  StringGrid.Cols[1].Clear;
+  StringGrid.Cols[2].Clear;
+
+  StringGrid.Rows[0].Add('Código');
+  StringGrid.Rows[0].Add('Nome');
+  StringGrid.Rows[0].Add('Documento');
+
+  for c in FClientes do
+  begin
+    StringGrid.Cols[0].AddObject(c.Codigo.ToString, c);
+    StringGrid.Cols[1].Add(c.Nome);
+    StringGrid.Cols[2].Add(c.Documento);
+  end;
+
+  if selRow >= StringGrid.RowCount then
+    selRow := StringGrid.RowCount - 1;
+
+  StringGrid.Row := selRow;
+
+  if FClientes.Count > 0 then
+    TFactoryForm.setFormFromClasse(Self, getRegistroFromClienteList(selRow));
+
+end;
+
+function TForm3.getRegistroFromClienteList(LinhaGrid: Integer): TCliente;
+begin
+  Result := TCliente(StringGrid.Objects[0, LinhaGrid]);
+end;
+
+procedure TForm3.btnCancelarClick(Sender: TObject);
+begin
+  inherited;
+  TFactoryForm.setFormFromClasse(Self, getRegistroFromClienteList(StringGrid.Row));
+end;
+
+procedure TForm3.btnEditarClick(Sender: TObject);
+begin
+  inherited;
+  edNome.SetFocus;
+  edNome.SelectAll;
+end;
+
+procedure TForm3.btnExcluirClick(Sender: TObject);
+var
+  c: TCliente;
+  i: Integer;
+begin
+  inherited;
+  i := StringGrid.Row;
+  c := getRegistroFromClienteList(i);
+  FPercistenceClass.delete(c);
+  FClientes.Remove(c);
+  atualizaGrid(i);
+end;
+
+procedure TForm3.btnNovoClick(Sender: TObject);
+begin
+  inherited;
+  edCodigo.Text := '0';
+  edCredito.Text := '0';
+  edCodigo.SetFocus;
+  edCodigo.SelectAll;
+  StringGrid.RowCount := StringGrid.RowCount + 1;
+  StringGrid.Row := StringGrid.RowCount - 1;
+end;
+
+procedure TForm3.btnSalvarClick(Sender: TObject);
+var
+  C: TCliente;
+begin
+  ListBox1.Clear;
+  C := TCliente.Create;
+
+  TFactoryClass.getClasseDoForm(Self, C);
+  TValidador.validarClasse(C, ListBox1.Items);
+
+  if ListBox1.Items.Count > 0 then
+    raise Exception.Create('Encontrado Erros de preenchimento!');
+
+  case FEditMode of
+    emInsert:
+      FPercistenceClass.Insere(C);
+    emEdit:
+      FPercistenceClass.Altere(C);
+  end;
+
+  FPercistenceClass.GetLista<TCliente>(FClientes);
+  atualizaGrid(StringGrid.Row);
+
+  inherited;
+end;
+
+procedure TForm3.Button1Click(Sender: TObject);
+begin
+  inherited;
+  DM.gerarRelatorio(FClientes);
+end;
+
+procedure TForm3.Button2Click(Sender: TObject);
+begin
+  inherited;
+  TExport.exportarLista<TCliente>(FClientes);
+end;
+
+procedure TForm3.Edit1KeyUp(Sender: TObject; var Key: Word; Shift: TShiftState);
+begin
+  inherited;
+  if Key = VK_RETURN then
+  begin
+    FPercistenceClass.GetLista<TCliente>(FClientes, Edit1.Text);
+    atualizaGrid();
+  end;
+end;
+
+procedure TForm3.FormCreate(Sender: TObject);
+begin
+  FClientes := TClientes.Create;
+  FPercistenceClass := TPersistenciaClass.Create;
+end;
+
+procedure TForm3.FormShow(Sender: TObject);
+begin
+  inherited;
+  FPercistenceClass.GetLista<TCliente>(FClientes);
+  atualizaGrid;
+end;
+
+procedure TForm3.StringGridSelectCell(Sender: TObject; ACol, ARow: Integer; var CanSelect: Boolean);
+begin
+  if StringGrid.Objects[0, ARow] <> nil then
+    TFactoryForm.setFormFromClasse(Self, getRegistroFromClienteList(ARow));
+end;
+
+end.
+
